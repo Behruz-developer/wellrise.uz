173e9562caaeffeec5ca8bf07573450d6482e557
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-05-21 12:37:59 +0500

    to chek

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,26 +1,37 @@
 function toggleDropdown() {
-    var languageOptions = document.getElementById("languageOptions");
-    if (languageOptions.style.display === "none") {
+  var languageOptions = document.getElementById("languageOptions");
+  if (languageOptions.style.display === "none") {
       languageOptions.style.display = "block";
       document.addEventListener('click', closeDropdownOnOutsideClick);
-    } else {
+  } else {
       languageOptions.style.display = "none";
       document.removeEventListener('click', closeDropdownOnOutsideClick);
-    }
   }
-  
-  function selectLanguage(language) {
-    var selected = document.querySelector(".selected");
-    selected.textContent = language;
-    toggleDropdown();
-  }
-  
-  function closeDropdownOnOutsideClick(event) {
-    var languageSelector = document.querySelector('.language-selector');
-    if (!languageSelector.contains(event.target)) {
+}
+
+function selectLanguage(language) {
+  var selected = document.querySelector(".selected");
+  var options = document.querySelectorAll(".option");
+
+  // Tanlangan tilni yangilash
+  var currentLanguage = selected.textContent;
+  selected.textContent = language;
+
+  // Barcha variantlarni ko'rsatish va tanlangan tilni almashtirish
+  options.forEach(option => {
+      if (option.textContent === language) {
+          option.textContent = currentLanguage;
+      }
+  });
+
+  toggleDropdown();
+}
+
+function closeDropdownOnOutsideClick(event) {
+  var languageSelector = document.querySelector('.language-selector');
+  if (!languageSelector.contains(event.target)) {
       var languageOptions = document.getElementById("languageOptions");
       languageOptions.style.display = 'none';
       document.removeEventListener('click', closeDropdownOnOutsideClick);
-    }
   }
-  
+}
